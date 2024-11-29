4dde11345ccbdba26c62403f5aeff936c06059e8
shivendra shukla <shivendra17@bitbucket.org>
2024-11-29 21:21:32 +0530

    Initial commit

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends AbstractController
+{
+    public function login(AuthenticationUtils $authenticationUtils)
+    {
+    
+        // Get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // Last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+        dump($lastUsername); // Ensure username/email is correctly set
+        return $this->render('security/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error,
+        ]);
+    }
+
+    public function logout()
+    {
+        // Symfony handles the logout process automatically
+        throw new \Exception('This will be intercepted by Symfony.');
+    }
+}
